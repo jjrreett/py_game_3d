9256ca5a4f1a70de3ff5cceb3542c321df59255b
Jarrett Bullion <jbullion@astra.com>
2022-05-16 18:21:48 -0700

    continued work

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # from projection import Projection
 # import pygame as pg
 import numpy as np
+import pygame as pg
 
 
 # def create_objects(self):
@@ -40,6 +41,14 @@ def scale_homogenious_vec3(array):
     return array / array[3]
 
 
+BLACK = (0, 0, 0, 255)
+WHITE = (255, 255, 255, 100)
+RED = (255, 0, 0, 100)
+GREEN = (0, 255, 0, 100)
+BLUE = (0, 0, 255, 100)
+EMPTY = pg.Color(0, 0, 0, 0)
+
+
 class Window:
     def __init__(self):
         self.ASPECT_RATIO = 2.0
@@ -55,6 +64,8 @@ class Window:
         # self.clock = pg.time.Clock()
         self.scene = Scene()
 
+        self.scene.add_Camera()
+
         self.scene.add_object(PointObject(0, 1, 1, name="a"))
         self.scene.add_object(PointObject(0, 2, 1, name="b"))
         self.scene.add_object(PointObject(1, 2, 1, name="c"))
@@ -65,17 +76,58 @@ class Window:
         self.scene.add_object(PointObject(1, 2, 2, name="g"))
         self.scene.add_object(PointObject(1, 1, 2, name="h"))
 
-        self.scene.camera.translate(Vec3(0, 0, 1))
+        self.scene.camera.translate(Vec3(0, -5, 1.5))
         self.scene.camera.yaw(np.pi / 2)
+
+        self.screen = pg.display.set_mode(self.RES)
+        self.surface = pg.Surface((self.WIDTH, self.HEIGHT), pg.SRCALPHA)
+
+        self.event_handlers = []
+        self.clock = pg.time.Clock()
+
+    def attach_event_handler(self, handler):
+        self.event_handlers.append(handler)
+
+    def handle_events(self):
+        for event in pg.event.get():
+            for handler in self.event_handlers:
+                handler(event)
 
     def render(self):
         self.scene.render()
 
+    def draw(self):
+
+        self.screen.fill(pg.Color("darkslategray"))
+        self.surface.fill(EMPTY)
+        for name, object in self.scene.objects.items():
+            pg.draw.circle(
+                self.surface,
+                WHITE,
+                (
+                    object.points_screen_floats[0][0] * self.WIDTH,
+                    object.points_screen_floats[1][0] * self.HEIGHT,
+                ),
+                10,
+            )
+
+        self.screen.blit(self.surface, (0, 0))
+        pg.display.flip()
+
+    def run(self):
+        while True:
+            self.draw()
+            self.handle_events()
+            pg.display.set_caption(str(int(self.clock.get_fps())))
+            self.clock.tick(self.FPS)
+
 
 class Scene:
     def __init__(self):
-        self.camera = Camera()
-        self.objects = {}
+        self.objects: list[Object3D] = {}
+
+    def add_Camera(self, fov=np.pi / 6, AR=2):
+        self.camera = Camera(fov=fov, AR=AR)
 
     def render(self):
         # apply individual object movments and recalculate transformation matrix
@@ -97,6 +149,12 @@ class Scene:
             object.calculate_points_in_camera_space(inverse_camera_matrix)
             object.calculate_points_projected(camera_projection_matrix)
             object.points_on_plane = scale_homogenious_vec3(object.points_projected)
+            object.points_screen_floats = (
+                self.camera.convert_projected_points_to_screen_floats(
+                    object.points_on_plane
+                )
+            )
+            # print(object.points_screen_floats)
 
     def add_object(self, object):
         self.objects[object.name] = object
@@ -104,7 +162,7 @@ class Scene:
 
 class Object:
     def __init__(self):
-        self.pos = Vec3(0, 0, 0)
+        self.pos = Vec3(0.0, 0.0, 0.0)
         self.alpha = 0  # yaw angle (about z, how much x/y moves) z' = z
         self.beta = 0  # pitch angle (about y', how much x'/z' moves) y'' = y'
         self.gamma = 0  # roll angle (about x'', how much y''/z'' moves) x''' = x''
@@ -166,11 +224,24 @@ class Object:
 
 
 class Camera(Object):
-    def __init__(self):
+    def __init__(self, fov=np.pi / 6, AR=2):
         super().__init__()
+
+        self.FOV = fov
+        self.AR = AR
+        self.WIDTH = 2 * np.sin(self.FOV / 2) / 1
+        self.HEIGHT = self.WIDTH / self.AR
+
         self.projection_matrix = np.array(
             [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0]]
         )
+
+    def convert_projected_points_to_screen_floats(self, points):
+        points = points[1:3, :]
+        points = points / np.array([[self.WIDTH], [-self.HEIGHT]]) + np.array(
+            [[0.5], [0.5]]
+        )
+        return points
 
 
 class Object3D(Object):
@@ -178,6 +249,8 @@ class Object3D(Object):
         super().__init__()
         self.name = name
         self._points_raw = points  # n x 4 ndarray
+        self.points_on_plane = points
+        self.points_screen_floats = points
         self.lines = lines  # list[list[ints]]
 
     def calculate_points_in_camera_space(self, inverse_camera_transformation_matrix):
@@ -190,14 +263,18 @@ class Object3D(Object):
     def calculate_points_projected(self, camera_projection_matrx):
         self.points_projected = camera_projection_matrx @ self.points_camera_space
 
-    # def scale_w_to_1(self):
-
 
 class PointObject(Object3D):
     def __init__(self, x, y, z, name=""):
         super().__init__(name, Homogenious_Vec3(x, y, z), [])
 
 
+def handle_quit(event):
+    if event.type == pg.QUIT:
+        exit()
+
+
 if __name__ == "__main__":
     window = Window()
-    window.render()
+    window.attach_event_handler(handle_quit)
+    window.run()
